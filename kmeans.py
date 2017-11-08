74a1c5bf4a3541c6bb39635c911b41f561923776
Jeffrey Zhang <Jeffrey@Jeffreys-MacBook-Pro.local>
2017-11-08 03:17:02 -0500

    readData + Kmeans

---
+++ kmeans.py
@@ -0,0 +1,21 @@
+import numpy as numpy
+import sklearn.cluster as cl
+import readData as rd
+import matplotlib.pyplot as plt
+
+X = rd.readData("toydata/photos/")
+clusters = cl.KMeans(n_clusters = 4, random_state = 0)
+X_clusters = clusters.fit_predict(X)
+
+cluster_map = {}
+for i in xrange(len(X_clusters)):
+	print i
+	if X_clusters[i] not in cluster_map:
+		cluster_map[X_clusters[i]] = []
+	cluster_map[X_clusters[i]].append(i)
+
+print len(cluster_map[0])
+for i in cluster_map[0]:
+	a = X[i].reshape((200,200))
+	plt.imshow(a, cmap=plt.cm.gray)
+	plt.show()
---
+++ readData.py
@@ -0,0 +1,21 @@
+import numpy as np
+from scipy import misc
+from os import listdir
+from os.path import isfile, join
+
+def readData(mypath, shape = (200,200)):
+	onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+	X = []
+	i = 0
+	for f in onlyfiles[:500]:
+		print i
+		i+=1
+		a = misc.imread(mypath+f, mode = "L")
+		aspect_ratio = float(a.shape[0]) / a.shape[1]
+		if 0.5 < aspect_ratio < 2.0:
+			a = misc.imresize(a, shape)
+			if len(X) == 0:
+				X = a.flatten()
+			else:
+				X = np.vstack((X, a.flatten()))
+	return X
