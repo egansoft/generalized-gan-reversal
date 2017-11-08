3bd6ef78d0fe038dc481416960d07cf3548c3e0b
Nicholas Egan <nicholas@egansoft.com>
2017-11-08 04:45:44 -0500

    fixes

--- kmeans.py
+++ kmeans.py
@@ -1,9 +1,8 @@
 import numpy as numpy
 import sklearn.cluster as cl
-import readData as rd
-import matplotlib.pyplot as plt
+import transformData as rd
 
-X = rd.readData("toydata/photos/")
+X = rd.readData("data/photos/")
 clusters = cl.KMeans(n_clusters = 4, random_state = 0)
 X_clusters = clusters.fit_predict(X)
 
@@ -13,9 +12,3 @@ for i in xrange(len(X_clusters)):
   if X_clusters[i] not in cluster_map:
     cluster_map[X_clusters[i]] = []
   cluster_map[X_clusters[i]].append(i)
-
-print len(cluster_map[0])
-for i in cluster_map[0]:
-  a = X[i].reshape((200,200))
-  plt.imshow(a, cmap=plt.cm.gray)
-  plt.show()
