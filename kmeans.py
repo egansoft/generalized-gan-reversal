22f09a0d23c08fe88a4bac61689cc5bc33632742
Nicholas Egan <nicholas@egansoft.com>
2017-11-08 04:04:48 -0500

    retabbed

--- kmeans.py
+++ kmeans.py
@@ -9,13 +9,13 @@ X_clusters = clusters.fit_predict(X)
 
 cluster_map = {}
 for i in xrange(len(X_clusters)):
-	print i
-	if X_clusters[i] not in cluster_map:
-		cluster_map[X_clusters[i]] = []
-	cluster_map[X_clusters[i]].append(i)
+  print i
+  if X_clusters[i] not in cluster_map:
+    cluster_map[X_clusters[i]] = []
+  cluster_map[X_clusters[i]].append(i)
 
 print len(cluster_map[0])
 for i in cluster_map[0]:
-	a = X[i].reshape((200,200))
-	plt.imshow(a, cmap=plt.cm.gray)
-	plt.show()
+  a = X[i].reshape((200,200))
+  plt.imshow(a, cmap=plt.cm.gray)
+  plt.show()
