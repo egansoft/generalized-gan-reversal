3c4a55c2e6c5da031fb72e0ce4fb70f9661c11c4
Nicholas Egan <nicholas@egansoft.com>
2017-11-20 07:56:06 -0500

    less output

--- gan.py
+++ gan.py
@@ -156,9 +156,10 @@ def train(opt, netD, netG, dataloader, nz):
       D_G_z2 = output.data.mean()
       optimizerG.step()
 
-      print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
-          % (epoch, opt.niter, i, len(dataloader),
-           errD.data[0], errG.data[0], D_x, D_G_z1, D_G_z2))
+      if i % 10 == 0:
+        print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
+            % (epoch, opt.niter, i, len(dataloader),
+             errD.data[0], errG.data[0], D_x, D_G_z1, D_G_z2))
       if i % 100 == 0:
         vutils.save_image(real_cpu,
             '%s/real_samples.png' % opt.outf,
