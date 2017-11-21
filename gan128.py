cdb39a2b77b733c83ea17b02e48e0d72de3dde6e
Ubuntu <ubuntu@ip-172-31-20-249.us-east-2.compute.internal>
2017-11-21 03:44:54 +0000

    a bunch of dumb shit

--- gan128.py
+++ gan128.py
@@ -5,31 +5,35 @@ import torch.optim as optim
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
 from torch.autograd import Variable
-import random
 
 class Discriminator(nn.Module):
   def __init__(self, ngpu, nc, ndf, backup_file):
     super(Discriminator, self).__init__()
     self.ngpu = ngpu
     self.main = nn.Sequential(
-      # input is (nc) x 64 x 64
-      nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+      # input is (nc) x 128 x 128
+      nn.Conv2d(nc, ndf, 4, stride=2, padding=1, bias=False), 
       nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf) x 32 x 32
-      nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+      # state size. (ndf) x 64 x 64
+      nn.Conv2d(ndf, ndf * 2, 4, stride=2, padding=1, bias=False),
       nn.BatchNorm2d(ndf * 2),
       nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf*2) x 16 x 16
-      nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+      # state size. (ndf*2) x 32 x 32
+      nn.Conv2d(ndf * 2, ndf * 4, 4, stride=2, padding=1, bias=False),
       nn.BatchNorm2d(ndf * 4),
       nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf*4) x 8 x 8
-      nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+      # state size. (ndf*4) x 16 x 16 
+      nn.Conv2d(ndf * 4, ndf * 8, 4, stride=2, padding=1, bias=False),
       nn.BatchNorm2d(ndf * 8),
       nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf*8) x 4 x 4
-      nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+      # state size. (ndf*8) x 8 x 8
+      nn.Conv2d(ndf * 8, ndf * 16, 4, stride=2, padding=1, bias=False),
+      nn.BatchNorm2d(ndf * 16),
+      nn.LeakyReLU(0.2, inplace=True),
+      # state size. (ndf*16) x 4 x 4
+      nn.Conv2d(ndf * 16, 1, 4, stride=1, padding=0, bias=False),
       nn.Sigmoid()
+      # state size. 1
     )
     self.apply(weights_init)
     if backup_file != '':
@@ -49,25 +53,29 @@ class Generator(nn.Module):
     self.ngpu = ngpu
     self.main = nn.Sequential(
       # input is Z, going into a convolution
-      nn.ConvTranspose2d(     nz, ngf * 8, 4, 1, 0, bias=False),
+      nn.ConvTranspose2d(     nz, ngf * 16, 4, 1, 0, bias=False),
+      nn.BatchNorm2d(ngf * 16),
+      nn.ReLU(True),
+      # state size. (ngf*16) x 4 x 4
+      nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
       nn.BatchNorm2d(ngf * 8),
       nn.ReLU(True),
-      # state size. (ngf*8) x 4 x 4
+      # state size. (ngf*8) x 8 x 8
       nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
       nn.BatchNorm2d(ngf * 4),
       nn.ReLU(True),
-      # state size. (ngf*4) x 8 x 8
+      # state size. (ngf*4) x 16 x 16 
       nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
       nn.BatchNorm2d(ngf * 2),
       nn.ReLU(True),
-      # state size. (ngf*2) x 16 x 16
+      # state size. (ngf*2) x 32 x 32
       nn.ConvTranspose2d(ngf * 2,     ngf, 4, 2, 1, bias=False),
       nn.BatchNorm2d(ngf),
       nn.ReLU(True),
-      # state size. (ngf) x 32 x 32
+      # state size. (ngf) x 64 x 64
       nn.ConvTranspose2d(    ngf,      nc, 4, 2, 1, bias=False),
       nn.Tanh()
-      # state size. (nc) x 64 x 64
+      # state size. (nc) x 128 x 128
     )
     self.apply(weights_init)
     if backup_file != '':
@@ -95,7 +103,7 @@ def train(opt, netD, netG, dataloader, nz):
 
   input = torch.FloatTensor(opt.batchSize, 3, opt.imageSize, opt.imageSize)
   noise = torch.FloatTensor(opt.batchSize, nz, 1, 1)
-  fixed_noise = torch.FloatTensor(64, nz, 1, 1).normal_(0, 1)
+  fixed_noise = torch.FloatTensor(opt.batchSize, nz, 1, 1).normal_(0, 1)
   label = torch.FloatTensor(opt.batchSize)
   real_label = 1
   fake_label = 0
@@ -116,8 +124,6 @@ def train(opt, netD, netG, dataloader, nz):
   print 'Starting training'
   for epoch in range(opt.niter):
     for i, data in enumerate(dataloader, 0):
-      flipper = random.random() < .1
-
       ############################
       # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
       ###########################
@@ -128,10 +134,7 @@ def train(opt, netD, netG, dataloader, nz):
       if opt.cuda:
         real_cpu = real_cpu.cuda()
       input.resize_as_(real_cpu).copy_(real_cpu)
-      if not flipper:
-        label.resize_(batch_size).fill_(real_label)
-      else:
-        label.resize_(batch_size).fill_(fake_label)
+      label.resize_(batch_size).fill_(real_label)
       inputv = Variable(input)
       labelv = Variable(label)
 
@@ -144,10 +147,7 @@ def train(opt, netD, netG, dataloader, nz):
       noise.resize_(batch_size, nz, 1, 1).normal_(0, 1)
       noisev = Variable(noise)
       fake = netG(noisev)
-      if not flipper:
-        labelv = Variable(label.fill_(fake_label))
-      else:
-        labelv = Variable(label.fill_(real_label))
+      labelv = Variable(label.fill_(fake_label))
       output = netD(fake.detach())
       errD_fake = criterion(output, labelv)
       errD_fake.backward()
