212d1831d0f386fff785c267c0703e232e612de0
Ubuntu <ubuntu@ip-172-31-20-249.us-east-2.compute.internal>
2017-11-21 04:12:32 +0000

    walk shit

--- latentWalk.py
+++ latentWalk.py
@@ -4,13 +4,23 @@ import torch
 import torchvision.utils as vutils
 from torch.autograd import Variable
 
-savedParams = sys.argv[1]
-outputImagePath = sys.argv[2]
+if len(sys.argv) > 1:
+  savedParams = sys.argv[1]
+else:
+  savedParams = 'sampleGparams.pth'
+if len(sys.argv) > 2:
+  outputImagePath = sys.argv[2]
+else:
+  outputImagePath = 'output/walk.png'
+if len(sys.argv) > 3:
+  numSamples = sys.argv[3]
+else:
+  numSamples = 64
 
-ngpu, nc, nz, ngf, batch_size = 1, 3, 100, 64, 64 # lol
+ngpu, nc, nz, ngf = 1, 3, 100, 64, # lol
 G = gan.Generator(ngpu, nc, nz, ngf, savedParams)
 
-noise = torch.FloatTensor(batch_size, nz, 1, 1)
+noise = torch.FloatTensor(numSamples, nz, 1, 1)
 noise.normal_(0, 1)
 noisev = Variable(noise)
 fake = G(noisev)
