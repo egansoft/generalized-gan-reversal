6d5982547985799fa8fd5a15c4244f52c4a976cf
Ubuntu <ubuntu@ip-172-31-14-23.us-east-2.compute.internal>
2017-11-22 03:26:11 +0000

    latentWalk update

--- latentWalk.py
+++ latentWalk.py
@@ -5,12 +5,8 @@ import torch
 import torchvision.utils as vutils
 from torch.autograd import Variable
 
-def slerp(p0, p1, t):
-  omega = np.arccos(np.dot(p0/np.linalg.norm(p0), p1/np.linalg.norm(p1)))
-  so = np.sin(omega)
-  return np.sin((1.0-t)*omega) / so * p0 + np.sin(t*omega)/so * p1
 
-def reverse_slerp(p0, p1, t):
+def slerp(p0, p1, t):
   omega = np.arccos(np.dot(p0/np.linalg.norm(p0), p1/np.linalg.norm(p1)))
   so = np.sin(omega)
   return np.sin((1.0-t)*omega) / so * p0 + np.sin(t*omega)/so * p1
@@ -19,6 +15,28 @@ def sample_spherical(npoints, ndim=3):
   vec = np.random.randn(ndim, npoints)
   vec /= np.linalg.norm(vec, axis=0)
   return vec
+
+def interpolate(U):
+  u = point / np.linalg.norm(U)
+  v = np.resize(sample_spherical(1, 100), 100)
+  V = v * np.linalg.norm(U)
+  w = -(u + v) / np.linalg.norm(u + v)
+  W = w * np.linalg.norm(U)
+  interpolation = []
+  print(U)
+  print(V)
+  print(W)
+  slerps = np.linspace(0, 0.9999, 21)
+  slerps2 = np.linspace(0, 0.9999, 22)
+  for i in slerps:
+    interpolation.append(slerp(U, V, i))
+  for i in slerps:
+    interpolation.append(slerp(V, W, i))
+  for i in slerps2:
+    interpolation.append(slerp(W, U, i))
+  interpolation = np.concatenate(interpolation, axis = 0)
+  return interpolation.reshape(64,100,1,1)
+
 
 if len(sys.argv) > 1:
   savedParams = sys.argv[1]
@@ -54,6 +72,7 @@ interpolation = np.concatenate(interpolation, axis = 0)
 SLERP = interpolation.reshape(64,100,1,1)
 '''
 
+'''
 #Full Interpolation
 slerps = np.linspace(0, 0.99, 21)
 slerps2 = np.linspace(0, 0.99, 22)
@@ -76,6 +95,10 @@ for i in slerps2:
   interpolation.append(slerp(vec3, vec1, i))
 interpolation = np.concatenate(interpolation, axis = 0)
 SLERP = interpolation.reshape(64,100,1,1)
+'''
+point = sample_spherical(1, 100)
+point = 0.11 * np.resize(point, 100)
+SLERP = interpolate(point)
 
 
 noise = torch.from_numpy(SLERP)
